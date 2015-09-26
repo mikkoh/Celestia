4a5f3c83e5499b292b6dca744e44ed29d5358360
Amal Khandelwal <khandelwalamal@gmail.com>
2015-09-26 01:53:07 -0400

    Set-up build process.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Celestia
 3D Einsteinian gravity simulator.
+
+### Development
+Install [Node.js].
+
+Then, from the root, run
+
+```sh
+> npm install
+```
+
+After all the dependencies are installed, run
+
+```sh
+> gulp
+```
+
+to create the `build` directory.
+
+To then watch for any file updates, run
+
+```sh
+> gulp watch
+```
+
+In a second terminal, run
+
+```sh
+> npm start
+```
+
+to serve the `build` directory on `http://localhost:8080/`.
+
+
+### License
+
+MIT
+
+
+[//]: # (Reference Links)
+
+  [Node.js]: <https://nodejs.org/en/download/>
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+// Dependencies
+// -------------------------
+var gulp   = require('gulp');
+var jshint = require('gulp-jshint');
+var clean  = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+
+
+var bases = {
+    src: './src/',
+    build: './build/'
+};
+
+var paths = {
+    html: ['index.html'],
+    images: ['data/images/**/*.png'],
+    script: ['js/main.js'],
+    styles: ['css/**/*.css'],
+    extras: ['favicon.ico']
+};
+
+
+// Tasks
+// -------------------------
+
+gulp.task('jshint', function() {
+    gulp.src(bases.src + 'js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+// Delete the build directory
+gulp.task('clean', function() {
+    return gulp.src(bases.build)
+        .pipe(clean());
+});
+
+
+// Build tasks
+gulp.task('browserify', function() {
+    var b = browserify(bases.src + paths.script, {
+        debug: false
+    });
+    return b.bundle()
+        .pipe(source('app.browserified.js'))
+        .pipe(gulp.dest(bases.build + 'js/'))
+});
+
+gulp.task('uglify', ['browserify'], function() {
+    return gulp.src(bases.build + 'js/app.browserified.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest(bases.build + 'js/'));
+});
+
+
+// Copy all other files to build/ directly
+gulp.task('copy', ['uglify'], function() {
+    // Copy html
+    gulp.src(paths.html, { cwd: bases.src })
+        .pipe(gulp.dest(bases.build));
+
+    // Copy styles
+    gulp.src(paths.styles, { cwd: bases.src })
+        .pipe(gulp.dest(bases.build + 'css/'));
+
+    // Copy extras
+    gulp.src(paths.extras, { cwd: bases.src })
+        .pipe(gulp.dest(bases.build));
+});
+
+
+// Commands
+// -------------------------
+
+// A development task to run anytime a file changes
+gulp.task('watch', function() {
+    gulp.watch(bases.src + '**/*', ['copy']);
+});
+
+gulp.task('build', ['clean', 'uglify', 'copy']);
+
+// Define the default task as a sequence of the above tasks
+gulp.task('default', ['build']);
